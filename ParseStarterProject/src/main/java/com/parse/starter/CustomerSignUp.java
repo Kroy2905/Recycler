dcac25cb1c514fa09cbbc1ce97e310dd1a243264
Kroy2905 <67234728+Kroy2905@users.noreply.github.com>
2021-06-18 16:00:41 +0530

    Final Recycler2

--- ParseStarterProject/src/main/java/com/parse/starter/CustomerSignUp.java
+++ ParseStarterProject/src/main/java/com/parse/starter/CustomerSignUp.java
@@ -48,8 +48,8 @@ public class CustomerSignUp extends AppCompatActivity {
                     public void done(ParseException e) {
                         if (e == null) {
                             Toast.makeText(CustomerSignUp.this, "Sign Up Successful", Toast.LENGTH_LONG).show();
-                            /*Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                            startActivity(intent);*/
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
                         }
                         //Log.i("Success", "DONE!");
                         else
